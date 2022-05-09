b2d80cc3eb3929adc6c4917d0c01ecedbba924c1
Tim Menzel <tim_menzel@t-online.de>
2022-05-09 16:28:18 +0200

    added acident car

---
+++ web-client/src/assets/icons/repairService/RepairDevice.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg viewBox="0 0 14 35" xmlns="http://www.w3.org/2000/svg" fill="#0ff00000">
+    <path
+      class=""
+      stroke="#d1d1d1"
+      stroke-width="0.5px"
+      d="M5 9 5 26Q0 29 4 34L4 31 7 29 10 31 10 34Q14 29 9 26L9 9Q14 6 10 1L10 4 7 6 4 4 4 1Q0 6 5 9"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  components: {},
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style lang="scss"></style>
---
+++ web-client/src/assets/icons/repairService/RepairServiceCluster.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="repairClusterID" class="repairCluster_wrapper" @mouseover="setoverlay" @mouseleave="resetPos">
+    <CarGoodCondtionIcon id="repairClusterGoodID" class="repairCluster_good" />
+    <!--   <RepairDevice class="repairCluster_repairdevice" /> -->
+    <CarBadCondtionIcon class="repairCluster_bad" />
+  </div>
+</template>
+
+<script>
+import RepairDevice from "@/assets/icons/repairService/RepairDevice.vue";
+import CarGoodCondtionIcon from "@/assets/icons/repairService/CarGoodCondtionIcon.vue";
+import CarBadCondtionIcon from "@/assets/icons/repairService/CarBadCondtionIcon.vue";
+
+export default {
+  components: {
+    RepairDevice,
+    CarBadCondtionIcon,
+    CarGoodCondtionIcon,
+  },
+  data() {
+    return {
+      hover: false,
+      hoverDelay: false,
+    };
+  },
+  methods: {
+    setoverlay(evt) {
+      const parent = document.getElementById("repairClusterID");
+      const child = document.getElementById("repairClusterGoodID");
+
+      const posInPercent = (evt.x / parent.offsetWidth) * 100;
+      // console.log("pos", posInPercent);
+      child.setAttribute("style", `width:${posInPercent}%`);
+    },
+    resetPos() {
+      const child = document.getElementById("repairClusterGoodID");
+      child.setAttribute("style", "width:50%");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+/* .repairCluster_wrapper:hover .repairCluster_bad {
+  width: 100%;
+}
+.repairCluster_wrapper:hover .repairCluster_good {
+  width: 100%;
+} */
+
+.repairCluster {
+  &_wrapper {
+    height: 100%;
+    width: 100%;
+    overflow: hidden !important;
+    position: relative;
+    display: flex;
+  }
+
+  &_good {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50%;
+    min-height: 100%;
+    transition: all 0.1s;
+    background-color: rgb(255, 255, 255);
+    z-index: 5;
+  }
+
+  &_bad {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    min-height: 100%;
+  }
+
+  &_repairdevice {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+  }
+}
+</style>
