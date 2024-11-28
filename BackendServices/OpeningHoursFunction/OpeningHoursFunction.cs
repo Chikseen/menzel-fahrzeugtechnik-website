c8168790f01d918ca249e94386e4d5cd0ef41161
Tim Menzel <Tim_Menzel@t-online.de>
2024-11-28 14:11:39 +0100

    added cache and cors controll

--- BackendServices/OpeningHoursFunction/OpeningHoursFunction.cs
+++ BackendServices/OpeningHoursFunction/OpeningHoursFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using OpeningHoursFunction.Models;
 using OpeningHoursFunction.Services;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace OpeningHoursFunction;
@@ -10,9 +11,18 @@ public class OpeningHoursFunction(
     OpeningHoursService openingHoursService)
 {
     [Function(nameof(OpeningHoursFunction))]
-    public async Task<OpeningHours> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData request)
     {
         OpeningHours openingHours = await openingHoursService.GetOpeningHours();
-        return openingHours;
+
+        var response = request.CreateResponse(HttpStatusCode.OK);
+
+        await response.WriteAsJsonAsync(openingHours);
+        response.Headers.Add("Cache-Control", "max-age=259200");
+        response.Headers.Add("Access-Control-Allow-Origin", "*");
+        response.Headers.Add("Access-Control-Allow-Methods", "GET");
+        response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Your-Custom-Header");
+
+        return response;
     }
 }
